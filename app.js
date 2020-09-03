de6051f630346b382dc0370222d4d39f5fa83551
hintonbradley <hintonbradley@gmail.com>
2020-09-02 19:26:58 -0700

    added get routes

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 // Creating an instance of express
 const app = express();
 
+// Adding Routes:
+app.get('/posts', (req, res) => {
+    res.send('This is the posts page')
+})
+
+app.get('/', (req, res) => {
+    res.send('This is the home')
+})
+
+
+
 // Have the app listen for any incoming requests on a port
 // Create a callback to run when server starts up
 app.listen(7007, () => {
